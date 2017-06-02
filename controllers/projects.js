449d221715d945289c6b99e8d6fa8fa6c892239f
jigglor <andrewtsuji@gmail.com>
2017-06-01 18:44:01 -0700

    changed routes slightly

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,27 +2,27 @@ var express = require('express');
 var Project = require('../models/project');
 var router = express.Router();
 
-router.route('/')
-    //Get all projects. Need to add 'where' based on campus/class/tech
-    .post(function(req, res) {
-        Project.find(req.body.search, function(err, projects) {
+//Add a new project
+router.post('/', function(req, res) {
+    Project.findOne({ email: req.body.email }, function(err, project) {
+        if (project) return res.status(400).send({ message: 'Email already exists' });
+
+        Project.create(req.body, function(err, project) {
             if (err) return res.status(500).send(err);
 
-            return res.send(projects);
-        });
-    })
-    //Add a new project
-    .post(function(req, res) {
-        Project.findOne({ email: req.body.email }, function(err, project) {
-            if (project) return res.status(400).send({ message: 'Email already exists' });
-
-            Project.create(req.body, function(err, project) {
-                if (err) return res.status(500).send(err);
-
-                return res.send(project);
-            });
+            return res.send(project);
         });
     });
+});
+
+//Get all projects. Need to add 'where' based on campus/class/tech
+router.post('/search', function(req, res) {
+    Project.find(req.body, function(err, projects) {
+        if (err) return res.status(500).send(err);
+
+        return res.send(projects);
+    });
+});
 
 router.route('/:id')
     //Get a single project
@@ -35,16 +35,18 @@ router.route('/:id')
     })
     //Edit a project
     .put(function(req, res) {
-        Project.findByIdAndUpdate(req.params.id, req.body.update, { new: true}, function(err, result) {
+        Project.findByIdAndUpdate(req.params.id, req.body, { new: true}, function(err, result) {
             if (err) return res.status(500).send(err);
 
-            return (result)
+            return res.send(result);
         });
     })
     //Delete a project
     .delete(function(req, res) {
         Project.findByIdAndRemove(req.params.id, function(err) {
             if (err) return res.status(500).send(err);
+
+            res.send('Project Deleted');
         });
     });
 
